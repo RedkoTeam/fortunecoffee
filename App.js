84c6365e069ddaeebe7e896bbdc6281419e1505a
Shoapeng <74572500+shoapeng-redko@users.noreply.github.com>
2020-12-30 00:27:38 -0800

    Added payment section. Added expo browser

--- App.js
+++ App.js
@@ -321,6 +321,9 @@ import { Formik } from 'formik'
 import LoginChecker from './util/LoginChecker'
 import AsyncStorage from '@react-native-community/async-storage';
 
+// Web Broswer for payment
+import * as WebBrowser from 'expo-web-browser';
+
 
 ////////////////////
 // Styling  //
@@ -1223,171 +1226,68 @@ let ShopDatabase = [
   }
 ]
 
+
 function Payment({navigation, route}) {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [phone, setPhone] = useState('')
-  const [address, setAddress] = useState('')
-  const [city, setCity] = useState('')
-  const [country, setCountry] = useState('')
-  const [postal, setPostal] = useState('')
-  const [cityState, setCityState] = useState('')
-  const [cardNumber, setCardNumber] = useState('')
-  const [exp_month, setExp_Month] = useState('')
-  const [exp_year, setExp_Year] = useState('')
-  const [cvc, setCvc] = useState('')
+  const [result, setResult] = useState(null);
 
+  console.log(route)
 
-  function toStripe(name, email, phone, address, city, country, postal, cityState, cardNumber, exp_month, exp_year, cvc) {
-    fetch('https://peaceful-woodland-13730.herokuapp.com/api/customer', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({
-        name: name,
-        email: email,
-        phone: phone,
-        address: {
-          line1: address,
-          city: city,
-          country: country,
-          postal_code: postal,
-          state: cityState
-        },
-        subscription: route.params.subscription,
-        card: {
-          number: cardNumber,
-          exp_month: exp_month,
-          exp_year: exp_year,
-          cvc: cvc
-        }
-      })
-    }).then(response => response.json())
-      .then(data => {
-        console.log(data)
-        db.collection('users').doc(firebase.auth().currentUser.uid)
-        .set({
-          subscriptionLevel: route.params.subscription,
-          stripeId: data.subscription.customer,
-          subscriptionActive: true
-        }).then(() => {
-          console.log('yes')
-        }).catch(error => console.log(error))
-      });
-  }
+  const _handlePressButtonAsync = async () => {
+    console.log(route.params.subscription)
+    let subscriptionRoute = route.params.subscription;
+    // Switch cases for each subscription location 
+    switch(subscriptionRoute){
+      case "Amethyst": {
+        console.log("Amethyst selected")
+        let result = await WebBrowser.openBrowserAsync('https://payment-fortune-coffee.herokuapp.com');
+        setResult(result);
+        break;
+      }
+      case "Rose Quartz": {
+        console.log("Amethyst selected")
+        let result = await WebBrowser.openBrowserAsync('https://payment-fortune-coffee.herokuapp.com/rose');
+        setResult(result);
+        break;
+      } 
+      case "Sapphire": {
+        console.log("Amethyst selected")
+        let result = await WebBrowser.openBrowserAsync('https://payment-fortune-coffee.herokuapp.com/sapphire');
+        setResult(result);
+        break;
+      } 
+      case "Tiger's Eye": {
+        console.log("Amethyst selected")
+        let result = await WebBrowser.openBrowserAsync('https://payment-fortune-coffee.herokuapp.com/tiger');
+        setResult(result);
+        break;
+      }
+      default:{
+        
+      }
+    }
+    
+  };
 
+  useEffect(()=>{
+    const unsubscribe = navigation.addListener('focus', () => {
+      _handlePressButtonAsync();
+    });
+    return unsubscribe;
+  },[navigation])
 
   return (
- 
-    <View style={{  alignItems: 'center', height: '100%' ,justifyContent: 'center'}}>
-      <ImageBackground source={bgstars} style={styles.bgfull}>
-      <View style={styles.flexInRows}>
-          <TouchableOpacity onPress={() => navigation.navigate('Home')} style={{ top: 40, marginLeft: 10 }}>
+    <View style={styles.container}>
+       <TouchableOpacity onPress={()=>navigation.navigate('Home')} style={styles.backButtonStyle}>
             <Image source={backButton} />
-          </TouchableOpacity>
-      </View>
-        <Text></Text>
-        <Text style={{ color: '#FFFFFF', fontSize: 15, textAlign: 'left', alignSelf: 'stretch', marginLeft: 20, marginTop: 60 }}>Name</Text>
-        <TextInput style={styles.savedFortuneTextBox}
-          label="Name"
-          placeholder="   Enter name here"
-          placeholderTextColor='#DCDCDC'
-          onChangeText={name => setName(name)}
-        />
-        <Text style={{ color: '#FFFFFF', fontSize: 15, marginTop: 10, textAlign: 'left', alignSelf: 'stretch', marginLeft: 15 }}>Email</Text>
-        <TextInput style={styles.savedFortuneTextBox}
-          label="Email"
-          placeholder="   Enter Email Here"
-          placeholderTextColor='#DCDCDC'
-          onChangeText={email => setEmail(email)}
-        />
-        <Text style={{ color: '#FFFFFF', fontSize: 15, marginTop: 10, textAlign: 'left', alignSelf: 'stretch', marginLeft: 15 }}>Phone</Text>
-        <TextInput style={styles.savedFortuneTextBox}
-          label="Phone"
-          placeholder="   Enter Phone # Here"
-          placeholderTextColor='#DCDCDC'
-          onChangeText={phone => setPhone(phone)}
-        />
-        <Text style={{ color: '#FFFFFF', fontSize: 15, marginTop: 10, textAlign: 'left', alignSelf: 'stretch', marginLeft: 15 }}>Address</Text>
-        <TextInput style={styles.savedFortuneTextBox}
-          label="Address"
-          placeholder="   Address"
-          placeholderTextColor='#DCDCDC'
-          onChangeText={address => setAddress(address)}
-        />
-        <Text style={{ color: '#FFFFFF', fontSize: 15, marginTop: 10, textAlign: 'left', alignSelf: 'stretch', marginLeft: 15 }}>City</Text>
-        <TextInput style={styles.savedFortuneTextBox}
-          label="City"
-          placeholder="   City"
-          placeholderTextColor='#DCDCDC'
-          onChangeText={city => setCity(city)}
-        />
-        <Text style={{ color: '#FFFFFF', fontSize: 15, marginTop: 10, textAlign: 'left', alignSelf: 'stretch', marginLeft: 15 }}>Country</Text>
-        <TextInput style={styles.savedFortuneTextBox}
-          label="Country"
-          placeholder="   Country"
-          placeholderTextColor='#DCDCDC'
-          onChangeText={country => setCountry(country)}
-        />
-      <View style={{ flexDirection: 'row', justifyContent:'space-between' , width: '90%', height: '5%' ,marginTop:15}}>
-        <Text style={{ color: '#FFFFFF', fontSize: 15, marginTop: 10, textAlign: 'left', alignSelf: 'stretch' }}>Postal</Text>
-        <TextInput style={styles.savedFortuneTextBox2}
-          label="Postal"
-          placeholder="   Postal"
-          placeholderTextColor='#DCDCDC'
-          onChangeText={postal => setPostal(postal)}
-        />
-        <Text style={{ color: '#FFFFFF', fontSize: 15, marginTop: 10, textAlign: 'left', alignSelf: 'stretch'}}>State</Text>
-        <TextInput style={styles.savedFortuneTextBox2}
-          label="State"
-          placeholder="   State"
-          placeholderTextColor='#DCDCDC'
-          onChangeText={cityState => setCityState(cityState)}
-        />
-        </View>
-        <Text style={{ color: '#FFFFFF', fontSize: 15, marginTop: 10, textAlign: 'left', alignSelf: 'stretch', marginLeft: 15 }}>Credit Card</Text>
-        <TextInput style={styles.savedFortuneTextBox}
-          label="Credit Card"
-          placeholder="   Card Number"
-          placeholderTextColor='#DCDCDC'
-          onChangeText={cardNumber => setCardNumber(cardNumber)}
-        />
+        </TouchableOpacity> 
+      <Text>{result && JSON.stringify(result)}</Text>
+      
+      <TouchableOpacity onPress={()=>navigation.navigate('Home')} style={styles.backButtonStyle}>
+            <Text> Nice! You recieved fortunes and gems!</Text>
 
-        <Text style={{ color: '#FFFFFF', fontSize: 15, marginTop: 10, textAlign: 'left', alignSelf: 'stretch', marginLeft: 15 }}>Credit Card</Text>
-        <View style={{ flexDirection: 'row', width: '90%', height: '6%' }}>
-
-          <TextInput style={styles.savedFortuneTextBox2}
-            label="Month"
-            placeholder="      Month"
-            placeholderTextColor='#DCDCDC'
-          onChangeText={exp_month => setExp_Month(exp_month)}
-          />
-          <TextInput style={styles.savedFortuneTextBox2}
-            label="Year"
-            placeholder="      Year"
-            placeholderTextColor='#DCDCDC'
-            onChangeText={exp_year => setExp_Year(exp_year)}
-          />
-          <TextInput style={styles.savedFortuneTextBox3}
-            label="CVC"
-            placeholder="      CVC"
-            placeholderTextColor='#DCDCDC'
-            onChangeText={cvc => setCvc(cvc)}
-          />
-        </View>
-        <Text></Text>
-      <TouchableOpacity onPress={() => { 
-        toStripe(name, email, phone, address, city, country, postal, cityState, cardNumber, exp_month, exp_year, cvc)
-
-        }}>
-          <Image style={{marginTop:10 }} source={continueImage} />
-        </TouchableOpacity>
-      <Text></Text>
-   </ImageBackground>
+        </TouchableOpacity> 
     </View>
-    
-  )
+  );
   
 }
 
