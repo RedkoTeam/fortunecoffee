a9edcd4f34c3343b2be1acd66c5673f274d3a44c
redkoai <redko@theredko.com>
2021-01-01 21:41:27 -0800

    gem.js added

---
+++ src/screens/Gems.js
@@ -0,0 +1,27 @@
+import {useNavigation} from "@react-navigation/native";
+import React, {useEffect} from "react";
+import {Image, ImageBackground, Linking, View} from "react-native";
+import {TouchableOpacity} from 'react-native-gesture-handler';
+import styles from "../styles/styles";
+import backButton from "../../assets/FortuneCoffeePNGassets/reading/backButton.png";
+import gemsbg from "../../assets/FortuneCoffeePNGassets/Subscription/Gemsbg.png";
+
+function Gems() {
+    const navigation = useNavigation();
+    useEffect(()=>{
+        let mounted = true;
+    })
+    return (
+        <ImageBackground source={gemsbg} style={styles.bgfull}>
+                <View style={{ flex: 0.7, flexDirection: 'row', width: '100%', justifyContent: 'space-between', padding: 25, marginBottom:"50%" }}>
+                    <View style={{position:'absolute', top:0, flexDirection:'row', width:'100%', margin:10}}>
+                        <TouchableOpacity onPress={()=>{navigation.navigate('HomeScreen')}}>
+                            <Image source={backButton} style={styles.backButtonStyle}/>
+                        </TouchableOpacity>
+                    </View>
+                </View>
+        </ImageBackground>
+    )
+}
+
+export default Gems
