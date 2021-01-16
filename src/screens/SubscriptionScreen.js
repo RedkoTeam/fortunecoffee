16c8446f838adaeec0535267a33e0a3d5e0231ee
redkoai <redko@theredko.com>
2021-01-16 12:40:35 -0800

    subscriptions

--- src/screens/SubscriptionScreen.js
+++ src/screens/SubscriptionScreen.js
@@ -19,16 +19,15 @@ function SubscriptionScreen() {
     return (
         <View style={styles.virtualContainer}>
                 <ImageBackground source={subBackground} style={styles.virtualOne}>
-                <View style={{flexDirection: 'row', width: '100%', justifyContent: 'space-between', padding: 25, marginTop: 18 }}>
+                <View style={{flexDirection: 'row', width: '100%', justifyContent: 'space-between', padding: 15, marginTop: 5 }}>
                     <TouchableOpacity onPress={()=>navigation.navigate('Home')} style={{
                                 left:widthPercentageToDP(0),
-                                top:heightPercentageToDP(3)
+                                top:heightPercentageToDP(2)
                             }}>
                                     <Image source={backButton} />
                     </TouchableOpacity>
                 </View>
- 
-                    <Image source={subscriptionDescription} style={{width: widthPercentageToDP(70), height: heightPercentageToDP(25), marginTop: heightPercentageToDP(-6), resizeMode:'contain'}}/>
+                    <Image source={subscriptionDescription} style={{width: widthPercentageToDP(70), height: heightPercentageToDP(25), marginTop: heightPercentageToDP(-8), resizeMode:'contain'}}/>
                     <ScrollView>
                     <View style={{ flex: 1, flexDirection: 'column',alignItems: 'center', }}>
 
@@ -36,14 +35,14 @@ function SubscriptionScreen() {
                             <TouchableOpacity onPress={ () => navigation.navigate('Payment', {
                                 subscription: 'Amethyst'
                             })}>
-                                <Image source={sub1} style={{ width: widthPercentageToDP(80), height: heightPercentageToDP(12.3), resizeMode:'contain'}}/>
+                                <Image source={sub1} style={{ width: widthPercentageToDP(70), height: heightPercentageToDP(12.3), resizeMode:'contain',marginTop: heightPercentageToDP(2)}}/>
                             </TouchableOpacity>
 
 
                             <TouchableOpacity onPress={ () => navigation.navigate('Payment', {
                                  subscription: 'Rose Quartz'
                             })}>
-                                <Image source={sub2} style={{width: widthPercentageToDP(70), height: heightPercentageToDP(10.2), resizeMode:'contain', right: 3, marginTop: -4}}/>
+                                <Image source={sub2} style={{width: widthPercentageToDP(70), height: heightPercentageToDP(12.3), resizeMode:'contain', right: 6, marginTop: -4}}/>
                             </TouchableOpacity>
                             
                            
@@ -51,12 +50,12 @@ function SubscriptionScreen() {
                                 subscription: "Tiger's Eye"
                             })}>
 
-                                <Image source={sub3} style={{width: widthPercentageToDP(70), height: heightPercentageToDP(10), resizeMode:'contain', right: 3,marginTop: 4}}/>
+                                <Image source={sub3} style={{width: widthPercentageToDP(70), height: heightPercentageToDP(14), resizeMode:'contain', right: 3,marginTop: 4}}/>
                             </TouchableOpacity>
                             <TouchableOpacity onPress={() => navigation.navigate('Payment', {
                                 subscription: 'Sapphire'
                             })}>
-                                <Image source={sub4} style={{width: widthPercentageToDP(70), height: heightPercentageToDP(10), resizeMode:'contain', right: 3,marginTop: 1}}/>
+                                <Image source={sub4} style={{width: widthPercentageToDP(70), height: heightPercentageToDP(12.3), resizeMode:'contain', right: 3,marginTop: 1}}/>
                             </TouchableOpacity>
                         </View>
                         </ScrollView>
