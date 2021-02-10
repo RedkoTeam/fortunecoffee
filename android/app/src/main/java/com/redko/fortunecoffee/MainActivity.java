b88a5859c27886c4d4ff0127f403ba6dd651614d
shoapeng-redko <74572500+shoapeng-redko@users.noreply.github.com>
2021-02-09 20:58:45 -0800

    Revert "changes"
    This reverts commit 6808da0d582919d719e3e74d90f53f4d42ade52d, reversing
    changes made to 0ac9375db2e952ea9689de7a423fe651a8f8be6b.

--- android/app/src/main/java/com/redko/fortunecoffee/MainActivity.java
+++ android/app/src/main/java/com/redko/fortunecoffee/MainActivity.java
@@ -16,11 +16,7 @@ public class MainActivity extends ReactActivity {
     super.onCreate(savedInstanceState);
     // SplashScreen.show(...) has to be called after super.onCreate(...)
     // Below line is handled by '@expo/configure-splash-screen' command and it's discouraged to modify it manually
-<<<<<<< HEAD
     SplashScreen.show(this, SplashScreenImageResizeMode.CONTAIN, ReactRootView.class, false);
-=======
-    SplashScreen.show(this, SplashScreenImageResizeMode.COVER, ReactRootView.class, false);
->>>>>>> 847a05027d2096ec0daf767dbc91b59529e030bb
   }
 
 
